f0eaa3207e026d5960c26ebab4c47ecf8ba22711
Nathan Sass <nsass711@gmail.com>
2016-03-09 11:26:12 +0630

    initial

---
+++ SwiftyThings.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var message1 = "Hello World!"
+var message2 = "You are a beautiful place!!"
+message1.uppercaseString + " " + message2
